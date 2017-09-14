a26ba861e52b52f17b9a6d1123705c916a774331
Prateek <prateeks@thoughtworks.com>
2017-09-15 02:20:54 +0530

    Added Insertion Sort

---
+++ src/com/srt/sort/InsertionSort.java
@@ -0,0 +1,29 @@
+package com.srt.sort;
+
+import java.util.Arrays;
+import java.util.function.BiFunction;
+
+public class InsertionSort<T extends Comparable> implements Sort<T> {
+
+    private T[] Sort(T[] arr, BiFunction<T,T, Boolean> compare){
+        T[] array = Arrays.copyOf(arr, arr.length);
+        for (int i = 1; i < array.length; i++) {
+            T key = array[i];
+            int index = i;
+            while(index  > 0 && compare.apply(key, array[index -1])){
+                array[index] = array[index - 1];
+                index--;
+            }
+            array[index] = key;
+        }
+        return array;
+    }
+
+    public T[] SortAscending(T[] array){
+        return Sort(array, (a, b) -> a.compareTo(b) < 0);
+    }
+
+    public T[] SortDescending(T[] array) {
+        return Sort(array, (a, b) -> a.compareTo(b) > 0);
+    }
+}
---
+++ src/com/srt/sort/Sort.java
@@ -0,0 +1,7 @@
+package com.srt.sort;
+
+public interface Sort<T>{
+    T[] SortAscending(T[] array);
+
+    T[] SortDescending(T[] array);
+}
